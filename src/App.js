964e5c434ff153efb14084045e8838e975d561ea
Le Vuong Khanh <khanhlv146@gmail.com>
2023-10-06 16:34:17 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,24 +9,24 @@ function App() {
   const[adress, setAdress]= useState('');
   const[todos,setTodos]= useState([
     {
-      id: 'todo1',title:'watch'
+      id: 'todo1',title:'watch',type:'nice'
     },
     {
-    id: 'todo2',title:'learn'
+    id: 'todo2',title:'learn',type:'nice'
 
     },    {
-      id: 'todo3',title:'play'
+      id: 'todo3',title:'play',type:'bad'
   
       }
   ])
   const handleEventClick =(event)=>{
     if(!adress)
     {
-      alert("")
+      alert("ds")
       return;
     }
 //hook not merge state?
-let newTodo ={id: 'abc', title:adress}
+let newTodo ={id: 'abc', title:adress, id: 'vuong khanh'}
 setTodos([...todos,newTodo])
 setAdress('')
   }
@@ -47,8 +47,12 @@ setAdress('')
           <h2> Hello {name}</h2>
         </p>
         <Todo
-        myData ={todos}
+        todos ={todos}
         title={'All to do'}
+        />
+                <Todo
+        todos ={todos.filter(item => item.type ==='nice')}
+        title={'nice to do'}
         />
          <input type="text " value ={adress} onChange={(event)=> handleOnChangeIput(event)} ></input> 
        <button type ="button" onClick={(event)=>{handleEventClick(event)}}>  click me</button>
--- src/component/todo.js
+++ src/component/todo.js
@@ -1,15 +1,20 @@
 const Todo =(props)=>{
-    console.log(">>check prop: ", props)
-     const todos = props.myData;
-    return(
-        <div className='todo-container'>
-        {todos.map(todo => {
-            console.log("check",todo)
-          return(
-            <li className='todo-child' key={todo.id}>{todo.title}</li>
-          )
-        })}
-      </div>
-    )
+  console.log(">>check prop: ", props)
+   const todos = props.todos;
+  return(
+      <div className='todo-container'>
+        <div className="title">
+          {props.title}
+        </div>
+      {todos.map(todo => {
+          console.log("check",todo)
+        return(
+          <li className='todo-child' key={todo.id}>{todo.title}</li>
+        )
+      })}
+          <hr></hr>
+    </div>
+
+  )
 }
 export default Todo;    
