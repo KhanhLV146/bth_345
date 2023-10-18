b12e460d17c896401c569db0ee762beeb9134a4c
Le Vuong Khanh <khanhlv146@gmail.com>
2023-10-18 16:23:35 +0700

    upadte login

--- src/App.js
+++ src/App.js
@@ -4,40 +4,47 @@ import './App.css';
 import Nav from './component/Nav';
 import Covid from './component/covid';
 import LoginFormPopup from './component/LoginFormPopup'; 
-
+import LoginHook from "./component/LoginHook";
+import LogoutHook from "./component/LogoutHook";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showLoginPopup, setShowLoginPopup] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [showLoginPopup, setShowLoginPopup] = useState(false);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  }
+  // const handleLogin = () => {
+  //   setIsLoggedIn(true);
+  // }
 
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-  }
+  // const handleLogout = () => {
+  //   setIsLoggedIn(false);
+  // }
 
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
-        <div className="login-logout">
+        {/* <div className="login-logout">
           {isLoggedIn ? (
             <button onClick={handleLogout}>Logout</button>
           ) : (
             <button onClick={() => setShowLoginPopup(true)}>Login</button>
           )}
-        </div>
-        <img src={logo} className="App-logo" alt="logo" />
+        </div> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h2>Covid Tracking</h2>
         <Covid />
-        {!isLoggedIn && showLoginPopup && (
+        {/* {!isLoggedIn && showLoginPopup && (
           <LoginFormPopup
             onClose={() => setShowLoginPopup(false)}
             onLogin={handleLogin}
           />
-        )}
+        )} */}
+      <h2>The Hooks way</h2>
+      <LoginHook />
+      <LogoutHook />
+      <br />
+
+
       </header>
     </div>
   );
--- src/component/LoginFormPopup.js
+++ src/component/LoginFormPopup.js
@@ -1,43 +1,43 @@
-  import React, { useState } from "react";
-  import '../LoginFormPopup.css'
+  // import React, { useState } from "react";
+  // import '../LoginFormPopup.css'
 
-  function LoginFormPopup({ onClose, onLogin }) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  // function LoginFormPopup({ onClose, onLogin }) {
+  //   const [username, setUsername] = useState("");
+  //   const [password, setPassword] = useState("");
 
-    const handleLogin = () => {
+  //   const handleLogin = () => {
       
-      if (username === "user" && password === "password") {
-        onLogin();
-        onClose(); 
-      } else {
-        alert("email hoặc mật khẩu không đúng");
-      }
-    };
+  //     if (username === "user" && password === "password") {
+  //       onLogin();
+  //       onClose(); 
+  //     } else {
+  //       alert("email hoặc mật khẩu không đúng");
+  //     }
+  //   };
 
-    return (
-      <div className="login-popup">
-        <div className="login-form">
-          <h2>Đăng nhập</h2>
+  //   return (
+  //     <div className="login-popup">
+  //       <div className="login-form">
+  //         <h2>Đăng nhập</h2>
     
-          <input
-            type="text"
-            placeholder="email"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+  //         <input
+  //           type="text"
+  //           placeholder="email"
+  //           value={username}
+  //           onChange={(e) => setUsername(e.target.value)}
+  //         />
 
-          <input
-            type="password"
-            value={password}
-            placeholder="Mật khẩu"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={handleLogin}>Đăng nhập</button>
-          <button onClick={onClose}>Đóng</button>
-        </div>
-      </div>
-    );
-  }
+  //         <input
+  //           type="password"
+  //           value={password}
+  //           placeholder="Mật khẩu"
+  //           onChange={(e) => setPassword(e.target.value)}
+  //         />
+  //         <button onClick={handleLogin}>Đăng nhập</button>
+  //         <button onClick={onClose}>Đóng</button>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
-  export default LoginFormPopup;
+  // export default LoginFormPopup;
